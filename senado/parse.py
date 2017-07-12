88e6665a62b37e64750c4259c02bb4fd042873f5
Lucas Simon <lucassrod@gmail.com>
2017-07-11 23:26:05 -0300

    Cria classe para parsear os dados JSON

--- senado/parse.py
+++ senado/parse.py
@@ -1,2 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
+
+class SenadoParse(object):
+    """
+    Parse dos dados vindo do response
+    """
+
+    @staticmethod
+    def get_all(response):
+        """
+        Parse de todos os dados
+        """
+
+        return None
